fbd6db3fb2e3fb3192458537674bf781ddcd8473
Tueloper <ozurumbatochukwu@yahoo.com>
2020-10-22 09:51:31 +0100

    :sun_with_face: folder setup

---
+++ src/test/index.js
@@ -0,0 +1,9 @@
+import chai from 'chai';
+
+const { expect } = chai;
+
+describe('Initial test', () => {
+  it('should be equal to whoisinjail-backend', () => {
+    expect('whoisinjail-backend').to.equal('whoisinjail-backend');
+  });
+});
